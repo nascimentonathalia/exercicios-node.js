83f9cf359ba0632558ffc815750fcf9ab1968505
unknown <souza.nascimento.nathalia@gmail.com>
2022-08-23 09:55:06 -0300

    primeiro exercicio

---
+++ exercicio 1/index.js
@@ -0,0 +1,12 @@
+console.log("Olá mundo!");
+
+console.log("Estudando node.js");
+
+var aluna = "Nathalia Nascimento";
+var idade = 30 ;
+var endereco = "Rua José Noya, Recife-PE";
+
+console.log("Dados da Nathália: ");
+console.log("Aluna: "+ aluna);
+console.log("Idade: " + idade + " anos");
+console.log("Endereço: "+ endereco);
